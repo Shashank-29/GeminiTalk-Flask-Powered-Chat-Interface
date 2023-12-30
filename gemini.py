b0270616ac39ff0a1cbebc0d7c6f430f1c6d9556
P R Shashank <60494944+Shashank-29@users.noreply.github.com>
2023-12-30 15:54:19 +0530

    Add files via upload

---
+++ gemini.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+
+app = Flask(__name__)
+load_dotenv()
+
+# Configure generative AI
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Initialize generative model and chat
+model = genai.GenerativeModel("gemini-pro")
+chat = model.start_chat(history=[])
+chat_history = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', last_bot_response=None)
+
+@app.route('/', methods=['POST'])
+def ask_question():
+    global chat_history
+    if request.method == 'POST':
+        input_text = request.form['input']
+        if input_text:
+            response = chat.send_message(input_text)
+            chat_history.append(('You', input_text))
+            chat_history.extend([('Bot', response.text) ])
+
+            # Get the last response from the bot
+            last_bot_response = [chunk.text for chunk in response][-1]
+
+            return render_template('index.html', input=input_text, chat_history=chat_history, last_bot_response=last_bot_response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
